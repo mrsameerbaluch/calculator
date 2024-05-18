378eabae0daf7bae8adec7e194615b163b102f18
Sameer Ahmed <154733691+mrsameerbaluch@users.noreply.github.com>
2024-05-18 09:16:26 +0500

    Add files via upload
     new

---
+++ app.js
@@ -0,0 +1,29 @@
+function display(value) {
+    const display = document.getElementById('display');
+    if (display.value === '0' && value !== '.') {
+    display.value = value;
+    } else {
+    display.value += value;
+    }
+}
+
+function displayclear() {
+    document.getElementById('display').value = '';
+}
+
+function del() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+    if (display.value === '') {
+    display.value = '0';
+    }
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+    display.value = eval(display.value);
+    } catch {
+    display.value = 'Error';
+    }
+}
